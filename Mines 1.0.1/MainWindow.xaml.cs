65292342035628195c8a830ea194f68a6f510744
Karel Cveček <Cvecek.Karel@stud.vassboskovice.cz>
2021-12-25 16:39:35 +0100

    hej more progres jak sviň

--- Mines 1.0.1/MainWindow.xaml.cs
+++ Mines 1.0.1/MainWindow.xaml.cs
@@ -1,19 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Diagnostics;
-using System.Threading;
+﻿using System.Windows;
 
 namespace Mines_1._0._1
 {
@@ -22,21 +7,69 @@ namespace Mines_1._0._1
     /// </summary>
     public partial class MainWindow : Window
     {
+        Mines _1_1a = new Mines(false, 0);
+        Mines _1_2a =new Mines(false, 0);
+        Mines _1_3a = new Mines(false, 0);
+        Mines _1_4a = new Mines(false, 0);
+        Mines _1_5a = new Mines(false, 0);
+        Mines _1_6a = new Mines(false, 0);
+        Mines _1_7a = new Mines(false, 0);
+        Mines _1_8a = new Mines(false, 0);
+        Mines _1_9a = new Mines(false, 0);
+        Mines _1_10a = new Mines(false, 0);
+
+        Mines _2_1a = new Mines(false, 0);
+        Mines _2_2a = new Mines(false, 0);
+        Mines _2_3a = new Mines(false, 0);
+        Mines _2_4a = new Mines(false, 0);
+        Mines _2_5a = new Mines(false, 0);
+        Mines _2_6a = new Mines(false, 0);
+        Mines _2_7a = new Mines(false, 0);
+        Mines _2_8a = new Mines(false, 1);
+        Mines _2_9a = new Mines(false, 1);
+        Mines _2_10a = new Mines(false, 1);
+
+        Mines _3_1a = new Mines(false, 1);
+        Mines _3_2a = new Mines(false, 1);
+        Mines _3_3a = new Mines(false, 1);
+        Mines _3_4a = new Mines(false, 0);
+        Mines _3_5a = new Mines(false, 0);
+        Mines _3_6a = new Mines(false, 0);
+        Mines _3_7a = new Mines(false, 0);
+        Mines _3_8a = new Mines(false, 1);
+        Mines _3_9a = new Mines(true, 0);
+        Mines _3_10a = new Mines(false, 1);
+            
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+           
+            
+            
+            
+            
+           
+            
+            
+           
+            
         }
 
-        private void play_Click(object sender, RoutedEventArgs e)
+        public void _1_1_Click(object sender, RoutedEventArgs e)
         {
-            Stopwatch chas = new Stopwatch();
-            chas.Start();
-            do
-            {
-                
-                tmr.Content = chas.ro;
-            }
-            while (true);
+            _1_1.Content = _1_1a.MinesNear;
+
+        }
+
+        private void _1_2_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
+
+
 }
---
+++ Mines 1.0.1/Mines.cs
@@ -0,0 +1,29 @@
+﻿namespace Mines_1._0._1
+{
+    internal class Mines
+    {
+        public bool IsaMine { get; }
+        public int MinesNear { get; set; }
+        public string cordinates { get; }
+
+        public Mines(bool mine, int near)
+        {
+            this.IsaMine = mine;
+            this.MinesNear = near;
+
+        }
+        public void click( int mn)
+        {
+            if (this.IsaMine == true)
+            {
+                //explode
+            }
+            
+            
+        }
+        
+
+
+    }
+
+}
