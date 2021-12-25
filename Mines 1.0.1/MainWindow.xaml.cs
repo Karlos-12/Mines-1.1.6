d8c661c6b28fda3cae6c03924c2210eb36313641
Karel Cveček <Cvecek.Karel@stud.vassboskovice.cz>
2021-12-25 16:49:25 +0100

    12

--- Mines 1.0.1/MainWindow.xaml.cs
+++ Mines 1.0.1/MainWindow.xaml.cs
@@ -24,10 +24,10 @@ namespace Mines_1._0._1
         Mines _2_4a = new Mines(false, 0);
         Mines _2_5a = new Mines(false, 0);
         Mines _2_6a = new Mines(false, 0);
-        Mines _2_7a = new Mines(false, 0);
+        Mines _2_7a = new Mines(false, 1);
         Mines _2_8a = new Mines(false, 1);
         Mines _2_9a = new Mines(false, 1);
-        Mines _2_10a = new Mines(false, 1);
+        Mines _2_10a = new Mines(false, 0);
 
         Mines _3_1a = new Mines(false, 1);
         Mines _3_2a = new Mines(false, 1);
@@ -35,11 +35,88 @@ namespace Mines_1._0._1
         Mines _3_4a = new Mines(false, 0);
         Mines _3_5a = new Mines(false, 0);
         Mines _3_6a = new Mines(false, 0);
-        Mines _3_7a = new Mines(false, 0);
-        Mines _3_8a = new Mines(false, 1);
-        Mines _3_9a = new Mines(true, 0);
-        Mines _3_10a = new Mines(false, 1);
-            
+        Mines _3_7a = new Mines(false, 1);
+        Mines _3_8a = new Mines(true, 0);
+        Mines _3_9a = new Mines(false, 1);
+        Mines _3_10a = new Mines(false, 0);
+
+        Mines _4_1a = new Mines(false, 2);
+        Mines _4_2a = new Mines(true, 0);
+        Mines _4_3a = new Mines(false, 3);
+        Mines _4_4a = new Mines(false, 1);
+        Mines _4_5a = new Mines(false, 0);
+        Mines _4_6a = new Mines(false, 0);
+        Mines _4_7a = new Mines(false, 1);
+        Mines _4_8a = new Mines(false, 1);
+        Mines _4_9a = new Mines(false, 1);
+        Mines _4_10a = new Mines(false, 0);
+
+        Mines _5_1a = new Mines(false, 3);
+        Mines _5_2a = new Mines(true, 0);
+        Mines _5_3a = new Mines(true, 0);
+        Mines _5_4a = new Mines(false, 1);
+        Mines _5_5a = new Mines(false, 0);
+        Mines _5_6a = new Mines(false, 0);
+        Mines _5_7a = new Mines(false, 0);
+        Mines _5_8a = new Mines(false, 1);
+        Mines _5_9a = new Mines(false, 1);
+        Mines _5_10a = new Mines(false, 1);
+
+        Mines _6_1a = new Mines(false, 2);
+        Mines _6_2a = new Mines(true, 0);
+        Mines _6_3a = new Mines(false, 3);
+        Mines _6_4a = new Mines(false, 1);
+        Mines _6_5a = new Mines(false, 0);
+        Mines _6_6a = new Mines(false, 0);
+        Mines _6_7a = new Mines(false, 0);
+        Mines _6_8a = new Mines(false, 1);
+        Mines _6_9a = new Mines(true, 0);
+        Mines _6_10a = new Mines(false, 1);
+
+        Mines _7_1a = new Mines(false, 1);
+        Mines _7_2a = new Mines(false, 1);
+        Mines _7_3a = new Mines(false, 1);
+        Mines _7_4a = new Mines(false, 0);
+        Mines _7_5a = new Mines(false, 1);
+        Mines _7_6a = new Mines(false, 1);
+        Mines _7_7a = new Mines(false, 1);
+        Mines _7_8a = new Mines(false, 1);
+        Mines _7_9a = new Mines(false, 1);
+        Mines _7_10a = new Mines(false, 1);
+
+        Mines _8_1a = new Mines(false, 0);
+        Mines _8_2a = new Mines(false, 1);
+        Mines _8_3a = new Mines(false, 1);
+        Mines _8_4a = new Mines(false, 1);
+        Mines _8_5a = new Mines(false, 1);
+        Mines _8_6a = new Mines(true, 0);
+        Mines _8_7a = new Mines(false, 3);
+        Mines _8_8a = new Mines(false, 2);
+        Mines _8_9a = new Mines(false, 1);
+        Mines _8_10a = new Mines(false, 0);
+
+        Mines _9_1a = new Mines(false, 0);
+        Mines _9_2a = new Mines(false, 1);
+        Mines _9_3a = new Mines(true, 0);
+        Mines _9_4a = new Mines(false, 1);
+        Mines _9_5a = new Mines(false, 1);
+        Mines _9_6a = new Mines(false, 2);
+        Mines _9_7a = new Mines(true, 0);
+        Mines _9_8a = new Mines(true, 0);
+        Mines _9_9a = new Mines(false, 1);
+        Mines _9_10a = new Mines(false, 0);
+
+        Mines _10_1a = new Mines(false, 0);
+        Mines _10_2a = new Mines(false, 1);
+        Mines _10_3a = new Mines(false, 1);
+        Mines _10_4a = new Mines(false, 1);
+        Mines _10_5a = new Mines(false, 0);
+        Mines _10_6a = new Mines(false, 1);
+        Mines _10_7a = new Mines(false, 2);
+        Mines _10_8a = new Mines(false, 2);
+        Mines _10_9a = new Mines(false, 1);
+        Mines _10_10a = new Mines(false, 0);
+
 
 
         public MainWindow()
