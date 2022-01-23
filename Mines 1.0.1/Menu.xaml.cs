cd040f05de49d1a33216086a36dfdfddb6a76e55
Karel Cveček <Cvecek.Karel@stud.vassboskovice.cz>
2022-01-23 15:49:49 +0100

    menu uprgrade

---
+++ Mines 1.0.1/Menu.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mines_1._0._1
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MainWindow game = new MainWindow();
+            game.Show();
+            Hide();
+        }
+    }
+}
